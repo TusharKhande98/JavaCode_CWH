7eb4c5bf3fa38139638b802c7184ea530f3aabfe
Tushar Khande <90444989+TusharKhande98@users.noreply.github.com>
2023-10-06 15:13:29 +0530

    Add files via upload

---
+++ CWH_21CH5_Loops.java
@@ -0,0 +1,24 @@
+// WHILE LOOP
+
+public class CWH_21CH5_Loops {
+    public static void main(String[] args) {
+        System.out.println("here we print upto 100 by using loop");
+        int i =1;
+        while (i<6){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("finished running while loop");
+
+        int j =0;
+        while (j<=10){
+            System.out.println(j);
+            j += 2;
+        }
+        System.out.println("here we print 2,4,6,8,10");
+
+    }
+}
+
+
+
